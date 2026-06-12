17a6fcedaee59ebdb67f6b6e7c035269855e8594
antomurru07-ux <antomurru07@gmail.com>
2026-06-12 17:40:57 +0200

    Translate comments in crypto.h to English

--- sketch_sender/crypto.h
+++ sketch_sender/crypto.h
@@ -2,21 +2,21 @@
 #include <Arduino.h>
 
 /**
- * Inizializza il generatore di numeri casuali (DRBG) necessario per l'IV.
- * Chiamare una volta in setup().
- * @return true se OK, false in caso di errore
+ * Initialize the generator of random numbers (DRBG) necessary for IV.
+ * Call once in Setup()
+ * @return true if OK, false in case of error
  */
 bool crypto_init();
 
 /**
- * Cifra plain_text con AES-128 CBC + padding PKCS#7.
- * Il buffer out_buf conterrà: [IV 16 byte][ciphertext].
- * @return numero di byte scritti in out_buf, 0 se errore
+ * encrypt plain_text with AES-128 CBC + padding PKCS#7.
+ * The buffer out_buf contains: [IV 16 byte][ciphertext].
+ * @return number of byte written in out_buf, 0 if error
  */
 size_t encrypt_data(const char* plain_text, uint8_t* out_buf, size_t out_size);
 
 /**
- * Converte un array di byte in stringa esadecimale uppercase.
- * hex_out deve essere almeno len*2+1 byte.
+ * Convert an array of byte in a uppercase HEX string
+ * hex_out must be of at least len*2+1 byte.
  */
 void bytes_to_hex(const uint8_t* data, size_t len, char* hex_out);
