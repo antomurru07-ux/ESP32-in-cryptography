eeec6e52bd302bf772c24658ff4fceacc3640349
antomurru07-ux <antomurru07@gmail.com>
2026-06-12 17:41:47 +0200

    Update comments to English in espnow_manager.h

--- sketch_sender/espnow_manager.h
+++ sketch_sender/espnow_manager.h
@@ -2,8 +2,8 @@
 #include <Arduino.h>
 
 /**
- * Struttura del pacchetto inviato via ESP-NOW.
- * Deve essere identica sul ricevitore.
+ * Strucutre of the packet sent by ESP-NOW
+ * Must be identical on the receiver
  */
 typedef struct struct_message {
   int  counter;
@@ -11,15 +11,15 @@ typedef struct struct_message {
 } struct_message;
 
 /**
- * Inizializza WiFi in STA mode, ESP-NOW, PMK/LMK e aggiunge il peer.
- * @return true se OK
+ * Initialize WIFI in STA mode, ESP-NOW, PMK/LMK and add the peer
+ * @return true if OK
  */
 bool espnow_init();
 
 /**
- * Invia un pacchetto al ricevitore.
- * @param counter  numero progressivo
- * @param payload  stringa da inserire nel campo x
- * @return true se esp_now_send ha restituito ESP_OK
- */
+ * Send a paket at the receiver
+ * @param counter  progressive number
+ * @param payload  String to insert in the x field
+ * @return true if esp_now_send has ESP_OK as result
+  */
 bool espnow_send_data(int counter, const char* payload);
